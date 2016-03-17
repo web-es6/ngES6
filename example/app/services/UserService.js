8579336d353209772c615fb8561cf3c2b7b6ffc9
dchuangincar <dchuang@incardata.com.cn>
2016-03-17 15:33:06 +0800

    move example to another repository: https://github.com/web-es6/ngES6-example.git

--- example/app/services/UserService.js
+++
@@ -1,9 +0,0 @@
-import { Injector } from 'ngES6';
-
-export default class UserService extends Injector {
-    static $inject = ['$http'];
-
-    update() {
-        return 'from UserService';
-    }
-}
--- example/app/services/index.js
+++
@@ -1,7 +0,0 @@
-import { loader } from 'ngES6';
-
-const MODULE_NAME = 'es6.services';
-
-loader.services(require.context('./', true, /^(?!\.\/index\.js$).+\.js$/), MODULE_NAME);
-
-export default MODULE_NAME;
